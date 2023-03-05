093812c5f507ee6dbdf8ea7a2fcb25e5efc30415
dfabi <dfaiola18@gmail.com>
2023-03-05 14:53:20 +0100

    contact page

--- rm-web/src/app/entities/contact/contact.component.ts
+++ rm-web/src/app/entities/contact/contact.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Social {
+  url: string;
+  name: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+
+  public readonly picturePath: string = '../../../assets/pictures/contact.jpg';
+
+  socialList: Social[] = [
+    { url: '', name: 'renomajk', img: '../../../assets/images/fb.png' },
+    { url: '', name: '@renomajk', img: '../../../assets/images/insta.png' },
+    { url: '', name: '@renomajk', img: '../../../assets/images/tiktok.png' },
+  ];
 
 }
